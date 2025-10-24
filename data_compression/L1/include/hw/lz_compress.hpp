9c7350abedb09e65669aea0cb05e2a65ec7257d7
hongpeng Wu <1417984430@qq.com>
2025-10-25 00:35:30 +0800

    lzcompress减少初始化开销

--- data_compression/L1/include/hw/lz_compress.hpp
+++ data_compression/L1/include/hw/lz_compress.hpp
@@ -70,20 +70,20 @@ void lzCompress(hls::stream<ap_uint<8> >& inStream, hls::stream<ap_uint<32> >& o
         resetValue.range((i + 1) * c_dictEleWidth - 1, i * c_dictEleWidth + MATCH_LEN * 8) = -1;
     }
 // Initialization of Dictionary
-dict_flush:
+ dict_flush:
     for (int i = 0; i < LZ_DICT_SIZE; i++) {
 #pragma HLS PIPELINE II = 1
-#pragma HLS UNROLL FACTOR = 2
+#pragma HLS UNROLL FACTOR = 8
         dict[i] = resetValue;
     }
 
     uint8_t present_window[MATCH_LEN];
 #pragma HLS ARRAY_PARTITION variable = present_window complete
     for (uint8_t i = 1; i < MATCH_LEN; i++) {
-#pragma HLS PIPELINE off
+#pragma HLS PIPELINE II = 1
         present_window[i] = inStream.read();
     }
-lz_compress:
+ lz_compress:
     for (uint32_t i = MATCH_LEN - 1; i < input_size - LEFT_BYTES; i++) {
 #pragma HLS PIPELINE II = 1
 #pragma HLS dependence variable = dict inter false
@@ -150,14 +150,14 @@ lz_compress:
         outValue.range(31, 16) = match_offset;
         outStream << outValue;
     }
-lz_compress_leftover:
+ lz_compress_leftover:
     for (int m = 1; m < MATCH_LEN; m++) {
 #pragma HLS PIPELINE
         ap_uint<32> outValue = 0;
         outValue.range(7, 0) = present_window[m];
         outStream << outValue;
     }
-lz_left_bytes:
+ lz_left_bytes:
     for (int l = 0; l < LEFT_BYTES; l++) {
 #pragma HLS PIPELINE
         ap_uint<32> outValue = 0;
@@ -233,7 +233,7 @@ void lzCompress(hls::stream<IntVectorStream_dt<8, 1> >& inStream, hls::stream<In
         dict_flush:
             for (int i = 0; i < LZ_DICT_SIZE; i++) {
 #pragma HLS PIPELINE II = 1
-#pragma HLS UNROLL FACTOR = 2
+#pragma HLS UNROLL FACTOR = 8
                 dict[i] = resetValue;
             }
             resetDictFlag = false;
--- data_compression/L1/tests/lz4_compress/run_hls.tcl
+++ data_compression/L1/tests/lz4_compress/run_hls.tcl
@@ -17,7 +17,7 @@
 set CSIM 1
 set CSYNTH 1
 set COSIM 1
-set VIVADO_SYN 1
+set VIVADO_SYN 0
 set VIVADO_IMPL 0
 set CUR_DIR [pwd]
 set XF_PROJ_ROOT $CUR_DIR/../../..
@@ -65,5 +65,59 @@ if {$VIVADO_SYN == 1} {
 if {$VIVADO_IMPL == 1} {
   export_design -flow impl -rtl verilog
 }
+# Summarize Estimated Clock and Cosim Latency to compute T_exec
+set csynth_xml "$PROJ/$SOLN/syn/report/csynth.xml"
+set cosim_rpt "$PROJ/$SOLN/sim/report/lz4CompressEngineRun_cosim.rpt"
+set est_clk ""
+set lat_min ""
+set lat_avg ""
+set lat_max ""
+set total_cycles ""
+
+if {[file exists $csynth_xml]} {
+  set fp [open $csynth_xml r]
+  set data [read $fp]
+  close $fp
+  if {[regexp -line {<EstimatedClockPeriod>([0-9.]+)</EstimatedClockPeriod>} $data -> est]} {
+    set est_clk $est
+  }
+}
+
+if {[file exists $cosim_rpt]} {
+  set fp [open $cosim_rpt r]
+  set rpt [read $fp]
+  close $fp
+  # Parse the Verilog row
+  if {[regexp -line {\|\s*Verilog\|\s*Pass\|\s*([0-9]+)\|\s*([0-9]+)\|\s*([0-9]+)\|.*\|\s*([0-9]+)\|} $rpt -> lat_min lat_avg lat_max total_cycles]} {
+    # ok
+  }
+}
+
+set t_exec ""
+if {![string equal $est_clk ""] && ![string equal $lat_max ""]} {
+  set t_exec [format "%.3f" [expr {$est_clk * $lat_max}]]
+}
+
+# Write summary and copy reports
+file mkdir reports
+set summary_file "reports/summary_T_exec.txt"
+set sfp [open $summary_file w]
+puts $sfp "TargetClockPeriod = $CLKP ns"
+puts $sfp "EstimatedClockPeriod = $est_clk ns"
+puts $sfp "CosimLatency(max) = $lat_max cycles"
+puts $sfp "T_exec = EstimatedClockPeriod × CosimLatency(max) = $t_exec ns"
+close $sfp
+
+# Also write summary under sol1/syn/report for submission
+set sol_report_dir "$PROJ/$SOLN/syn/report"
+file mkdir $sol_report_dir
+set summary_file2 "$sol_report_dir/summary_T_exec.txt"
+set sfp2 [open $summary_file2 w]
+puts $sfp2 "TargetClockPeriod = $CLKP ns"
+puts $sfp2 "EstimatedClockPeriod = $est_clk ns"
+puts $sfp2 "CosimLatency(max) = $lat_max cycles"
+puts $sfp2 "T_exec = EstimatedClockPeriod × CosimLatency(max) = $t_exec ns"
+close $sfp2
+
 
 exit

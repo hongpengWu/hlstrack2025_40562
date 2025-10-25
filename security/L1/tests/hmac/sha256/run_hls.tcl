5ca32c01def4bf8c448aa0c80f45520573b65fe0
hongpeng Wu <1417984430@qq.com>
2025-10-25 20:36:00 +0800

    sha运行impl

--- security/L1/tests/hmac/sha256/run_hls.tcl
+++ security/L1/tests/hmac/sha256/run_hls.tcl
@@ -17,8 +17,8 @@
 set CSIM 1
 set CSYNTH 1
 set COSIM 1
-set VIVADO_SYN 0
-set VIVADO_IMPL 0
+set VIVADO_SYN 1
+set VIVADO_IMPL 1
 set CUR_DIR [pwd]
 set XF_PROJ_ROOT $CUR_DIR/../../../..
 set XPART xc7z020-clg484-1
@@ -27,7 +27,7 @@ set PROJ "hmac_sha256_test.prj"
 set SOLN "solution1"
 
 if {![info exists CLKP]} {
-    set CLKP "10.5"
+    set CLKP "10.1"
 }
 
 open_project -reset $PROJ
